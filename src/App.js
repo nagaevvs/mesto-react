25f3a594aa85c041a78f6acd296d8b2695557f1e
Vladimir Nagaev <v.s.nagaev@yandex.ru>
2022-02-11 21:31:23 +0300

    init react 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and tttttsave to reload.
+          Edit <code>src/App.js</code> and sagggggve to reload.
         </p>
         <a
           className="App-link"
